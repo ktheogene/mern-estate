46a2125dbe4469367517937ac026a5cb2637b2f6
akimanagabriel <gabsonbill@yahoo.com>
2024-01-28 08:36:25 +0200

    file add to git hub

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className='text-5xl  text-yellow-34523 text-opacity-20'> HELLO VITE AND REACT</h1>
+
+    </div>
+  )
+}
